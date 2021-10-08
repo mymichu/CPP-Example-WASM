79fd58e1005cba46c05e32814af09b2a0f098c49
Michel Meyer <michel.meyer@ypsomed.com>
2021-10-08 09:58:53 +0000

    add hello-world standalone and update wasm compiler

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 rm -rf build
 conan install --profile .conan-profile/emscripten --build missing -if build .
 conan build -if build -bf build .
-emrun --no_browser --port 8000 build/src/image-engine.html
+#emrun --no_browser --port 8000 build/src/image-engine.html
